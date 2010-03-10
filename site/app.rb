4d6023cf2ea85564eaa881ec4a277e71240f166d
Michael D. Hall <mhall@just3ws.com>
2010-03-10 14:44:25 -0600

    okay, now the file works-ish.

--- site/app.rb
+++ site/app.rb
@@ -6,6 +6,14 @@ rescue LoadError
   Bundler.setup
 end
 
+require 'sinatra'
+require 'sinatra/sequel'
+require 'sequel'
+require 'logger'
+require 'haml'
+require 'sass'
+
+
 #
 # Configuration
 #
@@ -13,52 +21,6 @@ end
 use Rack::Reloader
 use Rack::CommonLogger
 use Rack::ShowExceptions
-
-configure :development do
-  s = Session.create(:title => "Alt.NET", :description => "Learn everything about the Alt.NET and the community surrounding the movement.")
-  s = Session.create(:title => "All.NET")
-  s = Session.create(:title => "Not.NET")
-  puts "Added #{Session.count()} session(s)."
-  
-  s = Speaker.create(:handle => "spereira", :full_name => "Sergio Pereira", :biography => "Sergio is an avid JavaScripter and runs the Chicago Alt.NET developer group.")
-  s = Speaker.create(:handle => "sseely", :full_name => "Scott Seely", :biography => "Scott is a frequent speaker and author on all things .NET. He also runs the LCNUG in Grayslake, IL.")
-  s = Speaker.create(:handle => "just3ws", :full_name => "Michael D. Hall")
-  puts "Added #{Speaker.count()} speaker(s)."
-end
-
-#
-# Models
-#
-
-class Speaker < Sequel::Model  
-end
-
-class Session < Sequel::Model
-end
-
-
-
-
-# 
-# Home
-#
-
-get '/?' do
-	@title = build_title "Home"
-  haml :index
-end
-
-end
-
-#
-# Misc
-#
-
-get '/css/application.css' do
-  content_type 'text/css', :charset => 'utf-8'
-  sass :style
-end
-
 #
 # Migrations
 #
@@ -83,5 +45,46 @@ migration "create speakers table" do
     String :full_name, :null => false
     String :biography
   end
+end
+
+
+
+#
+# Models
+#
+
+class Speaker < Sequel::Model  
+end
+
+class Session < Sequel::Model
+end
+
+configure :development do
+  s = Session.create(:title => "Alt.NET", :description => "Learn everything about the Alt.NET and the community surrounding the movement.")
+  s = Session.create(:title => "All.NET")
+  s = Session.create(:title => "Not.NET")
+  puts "Added #{Session.count()} session(s)."
+  
+  s = Speaker.create(:handle => "spereira", :full_name => "Sergio Pereira", :biography => "Sergio is an avid JavaScripter and runs the Chicago Alt.NET developer group.")
+  s = Speaker.create(:handle => "sseely", :full_name => "Scott Seely", :biography => "Scott is a frequent speaker and author on all things .NET. He also runs the LCNUG in Grayslake, IL.")
+  s = Speaker.create(:handle => "just3ws", :full_name => "Michael D. Hall")
+  puts "Added #{Speaker.count()} speaker(s)."
+end
+
+# 
+# Home
+#
+
+get '/?' do
+  haml :index
+end
+
+#
+# Misc
+#
+
+get '/css/application.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :style
 end
 
