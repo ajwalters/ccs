8248210e338bfe65256753a54527e762afa9e127
Michael D. Hall <mhall@just3ws.com>
2010-03-12 10:32:50 -0600

    added pony gem

--- app.rb
+++ app.rb
@@ -9,12 +9,35 @@ rescue LoadError
 end
 
 require 'sinatra'
+require 'sinatra/sequel'
+require 'sequel'
 require 'haml'
 require 'sass'
 require 'rack'
 
+configure :development do
+  use Rack::Reloader
+end
 
-use Rack::Reloader
+set :database, ENV['DATABASE_URL'] || 'sqlite://ccs.db'
+
+puts "the submissions table doesn't exist."
+migration "create the submissions table" do
+  database.create_table :submissions do
+    primary_key :id
+    text :full_name
+    text :title
+    text :description
+    text :email
+    text :homepage
+    text :biography
+    timestamp :created_at
+    timestamp :updated_at
+  end
+end
+
+class Submission < Sequel::Model
+end
 
 # 
 # Home
@@ -25,7 +48,21 @@ get '/?' do
 end
 
 post '/submit/?' do
-  haml :speaker_submission_result
+  begin
+    Submission.insert(:full_name => params[:full_name].to_s,
+                      :title => params[:title].to_s,
+                      :description => params[:description].to_s,
+                      :email => params[:email].to_s,
+                      :homepage => params[:homepage].to_s,
+                      :biography => params[:biography].to_s,
+                      :created_at => Time.now,
+                      :updated_at => Time.now)
+    
+    haml :speaker_submission_result
+  rescue
+    "<html><head><title>Chicago Code Camp - Error</title></head><body>Whoops! We had a slight error.</body></html>"  
+  end
+  
 end
 
 
