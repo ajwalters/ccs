a7fd26469b7743dea78a1c96d93fd2b2fae96090
Michael D. Hall <mhall@just3ws.com>
2010-04-07 01:30:23 -0500

    there are inherint dangers of being exhausted.

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,8 +1,8 @@
-c/lass Submission < ActiveRecord::Base
+class Submission < ActiveRecord::Base
   include Permissions
   validates_presence_of :full_name, :biography, :email, :title, :description
 
   validates_length_of :biography, :description,
-                      :maximum =>500,
-                      :too_long =>"is too long. We're only taking {{count}} characters or less for now."
+    :maximum =>500,
+    :too_long =>"is too long. We're only taking {{count}} characters or less for now."
 end
