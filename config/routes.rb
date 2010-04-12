de9a5cf0ce1e6d23215ab8df1668e4cf993373c4
Michael D. Hall <mhall@just3ws.com>
2010-04-12 11:41:29 -0500

    updated layout, better IMO. new logo.

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :home
   map.directions 'directions', :controller => "home", :action => "directions"
   map.info "info", :controller => "home", :action => "info"
+  map.info "about", :controller => "home", :action => "about"
 
 
   # The priority is based upon order of creation: first created -> highest priority.
